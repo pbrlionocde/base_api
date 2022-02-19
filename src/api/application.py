80b244325a6193a9c5254dff81643993094841fb
pbrlionocde <gorbachenkomisha@gmail.com>
2022-02-19 16:30:32 +0200

    Fix CORS

--- src/api/application.py
+++ src/api/application.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from src.api.response_any_json.view import base_json_endpoint
 
 _app = Flask(__name__)
 _app.register_blueprint(base_json_endpoint)
+CORS(_app)
 
 
 def get_app():
